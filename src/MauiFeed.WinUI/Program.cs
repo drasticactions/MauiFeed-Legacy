c3224302b815209538bdc3daf63197696cdd6565
Timothy Miller <innerlogic4321@gmail.com>
2023-01-30 12:40:33 +0900

    Make single instance

---
+++ src/MauiFeed.WinUI/Program.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="Program.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System.Threading;
+using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+using Windows.ApplicationModel.Activation;
+using Windows.Storage;
+
+namespace MauiFeed.WinUI
+{
+    /// <summary>
+    /// Main Program.
+    /// </summary>
+    internal class Program
+    {
+        [STAThread]
+        private static async Task<int> Main(string[] args)
+        {
+            WinRT.ComWrappersSupport.InitializeComWrappers();
+            bool isRedirect = await DecideRedirection();
+            if (!isRedirect)
+            {
+                Microsoft.UI.Xaml.Application.Start((p) =>
+                {
+                    var context = new DispatcherQueueSynchronizationContext(
+                        DispatcherQueue.GetForCurrentThread());
+                    SynchronizationContext.SetSynchronizationContext(context);
+                    new App();
+                });
+            }
+
+            return 0;
+        }
+
+        private static async Task<bool> DecideRedirection()
+        {
+            bool isRedirect = false;
+            AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+            ExtendedActivationKind kind = args.Kind;
+            AppInstance keyInstance = AppInstance.FindOrRegisterForKey("randomKey");
+
+            if (keyInstance.IsCurrent)
+            {
+                keyInstance.Activated += OnActivated;
+            }
+            else
+            {
+                isRedirect = true;
+                await keyInstance.RedirectActivationToAsync(args);
+            }
+
+            return isRedirect;
+        }
+
+        private static void OnActivated(object? sender, AppActivationArguments args)
+        {
+            ExtendedActivationKind kind = args.Kind;
+        }
+    }
+}
